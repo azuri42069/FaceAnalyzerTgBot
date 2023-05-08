e9200c15dc7ec2248c9dd5d38f2500b0a232c2ee
Maksym Przyleski <87721232+azuri42069@users.noreply.github.com>
2023-05-08 07:50:13 +0600

    comments

--- bot.py
+++ bot.py
@@ -3,31 +3,31 @@ from deepface import DeepFace
 import configparser
 import random
 
-config = configparser.ConfigParser() 
+config = configparser.ConfigParser()                   #  api key parseng
 config.read("settings.ini")  
 
 api_key = config["TopSecret"]["API_KEY"]
 
 bot = telebot.TeleBot(api_key) 
 
-@bot.message_handler(content_types=['photo'])   
+@bot.message_handler(content_types=['photo'])             # handler for photos    
 def face_analyze(message):
     try:
-        fileID = message.photo[-1].file_id   
+        fileID = message.photo[-1].file_id                                     # get new photo (1 time)
         file_info = bot.get_file(fileID)
         downloaded_file = bot.download_file(file_info.file_path)
         with open("image.jpg", 'wb') as new_file:
             new_file.write(downloaded_file)
         
-        result_dict = DeepFace.analyze(img_path="image.jpg", actions=['age', 'gender', 'race', 'emotion'])
+        result_dict = DeepFace.analyze(img_path="image.jpg", actions=['age', 'gender', 'race', 'emotion'])       # main method
         age_level = result_dict[0]['age']
         dom_gn = result_dict[0]['dominant_gender']
         #dom_gn_level = result_dict[0]['gender'][dom_gn]
-        dom_rc = result_dict[0]['dominant_race']
+        dom_rc = result_dict[0]['dominant_race']                        # rzecz pospolitaia (раздел)
         #dom_rc_level = result_dict[0]['race'][dom_rc]
         dom_em = result_dict[0]['dominant_emotion']
         #dom_em_level = result_dict[0]['emotion'][dom_em]
-        real_age = int(age_level) - random.randint(14, 25)
+        real_age = int(age_level) - random.randint(14, 25)                                           # "real age"
         bot.send_message(message.chat.id, f"👶 Предположительный возраст: {real_age}-{age_level}")
         bot.send_message(message.chat.id, f"👥 Предположительный пол: {dom_gn}")
         bot.send_message(message.chat.id, f"👩🏻👦🏾Предположительная paca: {dom_rc}")
@@ -38,7 +38,7 @@ def face_analyze(message):
         bot.send_message(message.chat.id, "☠️ Неизвестная Ошибка (420)")
          
     
-@bot.message_handler(content_types = ['text'])
+@bot.message_handler(content_types = ['text'])              # handler for any text message  
 def bot_message(message):
     if message.chat.type == 'private':
         bot.send_message(message.chat.id, "Дайте нам лицо")
